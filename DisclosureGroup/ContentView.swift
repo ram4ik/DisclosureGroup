71cbba6397430edd7a4b2afa9b32eeaac00a28f5
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-07-18 21:28:46 +0300

    init

--- DisclosureGroup/ContentView.swift
+++ DisclosureGroup/ContentView.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var disclosure1 = false
+    @State private var disclosure2 = true
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        VStack {
+            DisclosureGroup("More Info", isExpanded: $disclosure1) {
+                RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                    .fill(Color.blue)
+                    .opacity(0.1)
+                    .overlay(Text("More Info!"))
+                    .frame(height: 200, alignment: .center)
+            }
+            DisclosureGroup("Accent Color", isExpanded: $disclosure2) {
+                Text("Accent Color")
+                    .font(.largeTitle)
+                    .padding(.all, 10)
+            }
+            .accentColor(.blue)
+            .padding(.all, 10)
+            .background(RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                            .fill(Color.blue).opacity(0.1))
+        }.padding(.horizontal, 10)
+        Spacer()
     }
 }
 
